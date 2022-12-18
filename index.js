f4c2ed0d6dfe2815e32f52ff5c2980ab2019d821
narzullayev0772 <alishernarzullayev7777@gmail.com>
2022-12-18 17:22:44 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const User = require("./model/user.model");
 const cors = require("cors");
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use("/auth", require("./router/auth.router"));
 
